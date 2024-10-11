1197e819b549be41b34077eef5a7cc8d206c2ed2
telvarost <telontelvarost@gmail.com>
2024-10-10 20:19:05 -0400

    Update to latest biny gradle and loom

--- src/main/java/com/github/telvarost/nowobtainablerecipes/events/init/RecipeListener.java
+++ src/main/java/com/github/telvarost/nowobtainablerecipes/events/init/RecipeListener.java
@@ -2,21 +2,13 @@ package com.github.telvarost.nowobtainablerecipes.events.init;
 
 import com.github.telvarost.nowobtainablerecipes.Config;
 import net.mine_diver.unsafeevents.listener.EventListener;
-import net.mine_diver.unsafeevents.listener.ListenerPriority;
-import net.minecraft.block.BlockBase;
-import net.minecraft.item.Block;
-import net.minecraft.item.ItemBase;
-import net.minecraft.item.ItemInstance;
-import net.minecraft.recipe.Recipe;
-import net.minecraft.recipe.RecipeRegistry;
-import net.minecraft.recipe.SmeltingRecipeRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.modificationstation.stationapi.api.event.recipe.RecipeRegisterEvent;
 import net.modificationstation.stationapi.api.recipe.CraftingRegistry;
-import net.modificationstation.stationapi.api.recipe.FuelRegistry;
 import net.modificationstation.stationapi.api.recipe.SmeltingRegistry;
 import net.modificationstation.stationapi.api.util.Identifier;
-
-import java.util.List;
 
 public class RecipeListener {
 
@@ -26,73 +18,73 @@ public class RecipeListener {
 
         if (type == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPED.type()) {
             if (Config.config.enableCobwebRecipe) {
-                CraftingRegistry.addShapedRecipe(new ItemInstance(BlockBase.COBWEB.asItem(), 1), "X X", " X ", "X X", 'X', ItemBase.string);
+                CraftingRegistry.addShapedRecipe(new ItemStack(Block.COBWEB.asItem(), 1), "X X", " X ", "X X", 'X', Item.STRING);
             }
 
             if (Config.config.enableCoalOreRecipes) {
-                CraftingRegistry.addShapedRecipe(new ItemInstance(BlockBase.COAL_ORE.asItem(), 1), "XXX", "XYX", "XXX", 'X', BlockBase.STONE, 'Y', ItemBase.coal);
+                CraftingRegistry.addShapedRecipe(new ItemStack(Block.COAL_ORE.asItem(), 1), "XXX", "XYX", "XXX", 'X', Block.STONE, 'Y', Item.COAL);
             }
 
             if (Config.config.enableRedstoneOreRecipe) {
-                CraftingRegistry.addShapedRecipe(new ItemInstance(BlockBase.REDSTONE_ORE.asItem(), 1), "XYX", "YYY", "XYX", 'X', BlockBase.STONE, 'Y', ItemBase.redstoneDust);
+                CraftingRegistry.addShapedRecipe(new ItemStack(Block.REDSTONE_ORE.asItem(), 1), "XYX", "YYY", "XYX", 'X', Block.STONE, 'Y', Item.REDSTONE);
             }
 
             if (Config.config.enableLapisOreRecipe) {
-                CraftingRegistry.addShapedRecipe(new ItemInstance(BlockBase.LAPIS_LAZULI_ORE.asItem(), 1), "XYX", "YYY", "XYX", 'X', BlockBase.STONE, 'Y', new ItemInstance(ItemBase.dyePowder, 1, 4));
+                CraftingRegistry.addShapedRecipe(new ItemStack(Block.LAPIS_ORE.asItem(), 1), "XYX", "YYY", "XYX", 'X', Block.STONE, 'Y', new ItemStack(Item.DYE, 1, 4));
             }
 
             if (Config.config.enableIronOreRecipe) {
-                CraftingRegistry.addShapedRecipe(new ItemInstance(BlockBase.IRON_ORE.asItem(), 1), "XXX", "XYX", "XXX", 'X', BlockBase.STONE, 'Y', ItemBase.ironIngot);
+                CraftingRegistry.addShapedRecipe(new ItemStack(Block.IRON_ORE.asItem(), 1), "XXX", "XYX", "XXX", 'X', Block.STONE, 'Y', Item.IRON_INGOT);
             }
 
             if (Config.config.enableGoldOreRecipe) {
-                CraftingRegistry.addShapedRecipe(new ItemInstance(BlockBase.GOLD_ORE.asItem(), 1), "XXX", "XYX", "XXX", 'X', BlockBase.STONE, 'Y', ItemBase.goldIngot);
+                CraftingRegistry.addShapedRecipe(new ItemStack(Block.GOLD_ORE.asItem(), 1), "XXX", "XYX", "XXX", 'X', Block.STONE, 'Y', Item.GOLD_INGOT);
             }
 
             if (Config.config.enableDiamondOreRecipe) {
-                CraftingRegistry.addShapedRecipe(new ItemInstance(BlockBase.DIAMOND_ORE.asItem(), 1), "XXX", "XYX", "XXX", 'X', BlockBase.STONE, 'Y', ItemBase.diamond);
+                CraftingRegistry.addShapedRecipe(new ItemStack(Block.DIAMOND_ORE.asItem(), 1), "XXX", "XYX", "XXX", 'X', Block.STONE, 'Y', Item.DIAMOND);
             }
 
             if (Config.config.enableSpongeRecipe) {
-                CraftingRegistry.addShapedRecipe(new ItemInstance(BlockBase.SPONGE.asItem(), 1), "XYX", "YZY", "XYX", 'X', ItemBase.slimeball, 'Y', new ItemInstance(BlockBase.WOOL, 1, 4), 'Z', ItemBase.bucket);
+                CraftingRegistry.addShapedRecipe(new ItemStack(Block.SPONGE.asItem(), 1), "XYX", "YZY", "XYX", 'X', Item.SLIMEBALL, 'Y', new ItemStack(Block.WOOL, 1, 4), 'Z', Item.BUCKET);
             }
         }
 
         if (type == RecipeRegisterEvent.Vanilla.SMELTING.type()) {
             if (Config.config.enableCoalOreRecipes) {
-                SmeltingRegistry.addSmeltingRecipe(new ItemInstance(BlockBase.COAL_ORE.asItem(), 1), new ItemInstance(ItemBase.coal, 1));
+                SmeltingRegistry.addSmeltingRecipe(new ItemStack(Block.COAL_ORE.asItem(), 1), new ItemStack(Item.COAL, 1));
             }
 
             if (Config.config.enableRedstoneOreRecipe) {
-                SmeltingRegistry.addSmeltingRecipe(new ItemInstance(BlockBase.REDSTONE_ORE.asItem(), 1), new ItemInstance(ItemBase.redstoneDust, 1));
+                SmeltingRegistry.addSmeltingRecipe(new ItemStack(Block.REDSTONE_ORE.asItem(), 1), new ItemStack(Item.REDSTONE, 1));
             }
 
             if (Config.config.enableLapisOreRecipe) {
-                SmeltingRegistry.addSmeltingRecipe(new ItemInstance(BlockBase.LAPIS_LAZULI_ORE.asItem(), 1), new ItemInstance(ItemBase.dyePowder, 1, 4));
+                SmeltingRegistry.addSmeltingRecipe(new ItemStack(Block.LAPIS_ORE.asItem(), 1), new ItemStack(Item.DYE, 1, 4));
             }
         }
 
         if (type == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPELESS.type()) {
             if (Config.config.enableAppleRecipe) {
-                CraftingRegistry.addShapelessRecipe(new ItemInstance(ItemBase.apple, 1), ItemBase.sugar, ItemBase.seeds, new ItemInstance(ItemBase.dyePowder, 1, 1), BlockBase.SAPLING);
+                CraftingRegistry.addShapelessRecipe(new ItemStack(Item.APPLE, 1), Item.SUGAR, Item.SEEDS, new ItemStack(Item.DYE, 1, 1), Block.SAPLING);
             }
 
             if (Config.config.enableDeadBushRecipe) {
-                CraftingRegistry.addShapelessRecipe(new ItemInstance(BlockBase.DEADBUSH.asItem(), 1), BlockBase.SAPLING, BlockBase.FIRE);
+                CraftingRegistry.addShapelessRecipe(new ItemStack(Block.DEAD_BUSH.asItem(), 1), Block.SAPLING, Block.FIRE);
             }
 
             if (Config.config.enableFireRecipe) {
-                CraftingRegistry.addShapelessRecipe(new ItemInstance(BlockBase.FIRE.asItem(), 3), ItemBase.flintAndSteel);
+                CraftingRegistry.addShapelessRecipe(new ItemStack(Block.FIRE.asItem(), 3), Item.FLINT_AND_STEEL);
             }
 
             if (Config.config.enableGrassRecipe) {
-                CraftingRegistry.addShapelessRecipe(new ItemInstance(BlockBase.GRASS.asItem(), 1), BlockBase.DIRT, ItemBase.seeds);
+                CraftingRegistry.addShapelessRecipe(new ItemStack(Block.GRASS_BLOCK.asItem(), 1), Block.DIRT, Item.SEEDS);
             }
 
             if (Config.config.enableIceRecipes) {
-                CraftingRegistry.addShapelessRecipe(new ItemInstance(BlockBase.ICE.asItem(), 1), ItemBase.waterBucket, BlockBase.SNOW_BLOCK, BlockBase.SNOW_BLOCK);
-                CraftingRegistry.addShapelessRecipe(new ItemInstance(BlockBase.ICE.asItem(), 2), ItemBase.waterBucket, BlockBase.SNOW_BLOCK, BlockBase.SNOW_BLOCK, ItemBase.waterBucket, BlockBase.SNOW_BLOCK, BlockBase.SNOW_BLOCK);
-                CraftingRegistry.addShapelessRecipe(new ItemInstance(BlockBase.ICE.asItem(), 3), ItemBase.waterBucket, BlockBase.SNOW_BLOCK, BlockBase.SNOW_BLOCK, ItemBase.waterBucket, BlockBase.SNOW_BLOCK, BlockBase.SNOW_BLOCK, ItemBase.waterBucket, BlockBase.SNOW_BLOCK, BlockBase.SNOW_BLOCK);
+                CraftingRegistry.addShapelessRecipe(new ItemStack(Block.ICE.asItem(), 1), Item.WATER_BUCKET, Block.SNOW_BLOCK, Block.SNOW_BLOCK);
+                CraftingRegistry.addShapelessRecipe(new ItemStack(Block.ICE.asItem(), 2), Item.WATER_BUCKET, Block.SNOW_BLOCK, Block.SNOW_BLOCK, Item.WATER_BUCKET, Block.SNOW_BLOCK, Block.SNOW_BLOCK);
+                CraftingRegistry.addShapelessRecipe(new ItemStack(Block.ICE.asItem(), 3), Item.WATER_BUCKET, Block.SNOW_BLOCK, Block.SNOW_BLOCK, Item.WATER_BUCKET, Block.SNOW_BLOCK, Block.SNOW_BLOCK, Item.WATER_BUCKET, Block.SNOW_BLOCK, Block.SNOW_BLOCK);
             }
        }
     }
